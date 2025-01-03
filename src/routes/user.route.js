bf176680941007237146462f663abef143bd204b
aaradhayasingh811 <aaradhayasingh811@gmail.com>
2025-01-03 20:10:39 +0530

    first commit

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,9 @@ import loginController from "../controllers/login.controller.js";
 
 router.route('/register').post(registerController);
 router.route('/login').post(loginController);
+router.route('/').get((res,req)=>{
+    res.setEncoding("Hello world");
+})
 
 
 export {router}
